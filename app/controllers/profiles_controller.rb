eee3d3da5e02a12675b130ea35cf476f8169bc69
Ponta-cloud <71808646+Ponta-cloud@users.noreply.github.com>
2021-05-22 17:08:08 +0900

    画像レスポンスの最後

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,8 @@ class ProfilesController < ApplicationController
   def home
     render :layout => nil
   end  
+
+  def contact
+    
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'profiles#home'
+  get '/contact', to: 'profiles#contact'
 end
